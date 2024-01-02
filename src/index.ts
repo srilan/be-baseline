5b8817c45d5729e6854156aa060bea21fafc248c
srilan <devd@Devs-MacBook-Pro.local>
2024-01-03 04:02:17 +0800

    fixed paths

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import startServer from './server';
 const PORT = process.env.PORT || 3000
 //import startServer from 'server';
 
@@ -14,31 +15,5 @@ const app = express();
 app.use(helmet()); 
 app.use(cors()); 
 app.use(express.json())
-
-
-const startServer = (app: express.Express) => {
-  //Listing to the app and running it on PORT 5000
-  app.listen(PORT, async () => {
-      console.log(`listning on port ${PORT}`)
-  });
-
-  /**
-   * Req: request coming from client to server
-   * Res: response from server to client
-   */
-  app.get('/list-stations', (req, res)=> {
-    res.json([
-      {
-        id: 1,
-        name: 'kamuning',
-      },
-      {
-        id: 2,
-        name: 'north ave',
-      }
-    ]).status(200);
-  })
-
-}
 
 startServer(app);
