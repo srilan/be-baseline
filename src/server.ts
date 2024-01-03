6c401bc240dfb9636a3d63e2238748820b4ee861
srilan <devd@Devs-MacBook-Pro.local>
2024-01-04 04:01:10 +0800

    a

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 //Importing Libraries 
 import express from 'express'
 import * as dotenv from 'dotenv';
+import { stations } from './stations';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
@@ -10,6 +11,43 @@ const startServer = (app: express.Express) => {
   //Listing to the app and running it on PORT 5000
   app.listen(PORT, async () => {
       console.log(`listning on port ${PORT}`)
+  })
+
+  app.get('/stations/list-stations', (req, res)=> {
+    res.send(
+      [
+        {
+          name: 'kamuning',
+          id: 1
+        }
+      ]
+    ).status(200)
+  })
+
+  app.get('/stations/get/:id', (req, res)=> {
+    const id = req.params['id'];
+    const station = stations.find((s)=>s.id===Number(id));
+    if (station) {
+      res.status(200).send(
+        station
+      )
+    } else {
+      res.status(500).send({
+        message: "Station not found"
+      });
+    }
+  })
+
+
+
+  app.post('/stations/add', (req, res)=> {
+    const stationName = 'kamuning';
+    const coodinates = '[1.0, 1.0]';
+
+
+    res.send({
+      message: "Station Has been added successfuly",
+    }).status(200);
   })
 
 }
---
+++ src/stations.ts
@@ -0,0 +1,19 @@
+
+
+export const stations = [
+  {
+    stationName: "kamuning",
+    id: 1,
+    coords: [1,1]
+  },
+  {
+    stationName: "cubao",
+    id: 2,
+    coords: [1,1]
+  },
+  {
+    stationName: "nortave",
+    id: 3,
+    coords: [1,1]
+  },
+]
